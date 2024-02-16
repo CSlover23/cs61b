1564c17e7e01a37ece90aa6aaa766a0bd788fbec
CSlover23 <csstudy23@outlook.com>
2024-02-16 23:09:19 +0800

    change a lot

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -9,39 +9,51 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> testArray = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> solArray = new ArrayDequeSolution<>();
         String message = "\n";
-        for (int i = 0; i < 10; i += 1) {
-            double numberBetweenZeroAndOne = StdRandom.uniform();
 
-            Integer input = StdRandom.uniform(1000);
+        for (int i = 0; i < 100; i += 1) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            Integer actual;
+            Integer expected;
+
             if (numberBetweenZeroAndOne < 0.5) {
-                testArray.addLast(input);
-                solArray.addLast(input);
-                Integer expected = solArray.get(testArray.size() - 1);
-                message += "addLast(" + expected + ")\n";
-                Integer actual = testArray.get(testArray.size() - 1);
+                testArray.addLast(i);
+                solArray.addLast(i);
+
+                actual = testArray.size();
+                expected = solArray.size();
+                message += "size()\n";
+                assertEquals(message, expected, actual);
+
+                actual = testArray.get(testArray.size() - 1);
+                expected = solArray.get(solArray.size() - 1);
+                message += "addLast(" + i + ")\n";
                 assertEquals(message, expected, actual);
             } else {
                 testArray.addFirst(i);
-                solArray.addLast(input);
-                Integer expected = solArray.get(0);
-                message += "addFirst(" + expected + ")\n";
-                Integer actual = testArray.get(0);
+                solArray.addFirst(i);
+                actual = testArray.get(0);
+                expected = solArray.get(0);
+
+                message += "addFirst(" + i + ")\n";
                 assertEquals(message, expected, actual);
             }
 
-            if (i == 5) {
-                Integer expected = solArray.get(i - 1);
-                Integer actual = testArray.removeLast();
-                message += "removeLast()\n";
-                assertEquals(message, expected, actual);
-            }
+            if (i % 3 == 0) {
+                actual = testArray.removeFirst();
+                expected = solArray.removeFirst();
 
-            if (i == 8) {
-                Integer expected = solArray.get(0);
-                Integer actual = testArray.removeFirst();
                 message += "removeFirst()\n";
                 assertEquals(message, expected, actual);
             }
+
+            if (i % 4 == 0) {
+                actual = testArray.removeLast();
+                expected = solArray.removeLast();
+
+                message += "removeLast()\n";
+                assertEquals(message, expected, actual);
+            }
         }
+
     }
 }

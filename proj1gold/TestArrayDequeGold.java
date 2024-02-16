d166f43206e12dfbdb27f419e3053038c68c3607
CSlover23 <csstudy23@outlook.com>
2024-02-16 21:05:53 +0800

    the second try

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,32 +7,37 @@ public class TestArrayDequeGold {
     public void studentTest() {
 
         StudentArrayDeque<Integer> testArray = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> solArray = new ArrayDequeSolution<>();
         String message = "\n";
         for (int i = 0; i < 10; i += 1) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
 
-            Integer expected = StdRandom.uniform(1000);
+            Integer input = StdRandom.uniform(1000);
             if (numberBetweenZeroAndOne < 0.5) {
-                testArray.addLast(expected);
+                testArray.addLast(input);
+                solArray.addLast(input);
+                Integer expected = solArray.get(testArray.size() - 1);
                 message += "addLast(" + expected + ")\n";
-                Integer actual = testArray.get(i);
+                Integer actual = testArray.get(testArray.size() - 1);
                 assertEquals(message, expected, actual);
             } else {
                 testArray.addFirst(i);
+                solArray.addLast(input);
+                Integer expected = solArray.get(0);
                 message += "addFirst(" + expected + ")\n";
                 Integer actual = testArray.get(0);
                 assertEquals(message, expected, actual);
             }
 
             if (i == 5) {
-                expected = testArray.get(i - 1);
+                Integer expected = solArray.get(i - 1);
                 Integer actual = testArray.removeLast();
                 message += "removeLast()\n";
                 assertEquals(message, expected, actual);
             }
 
             if (i == 8) {
-                expected = testArray.get(0);
+                Integer expected = solArray.get(0);
                 Integer actual = testArray.removeFirst();
                 message += "removeFirst()\n";
                 assertEquals(message, expected, actual);
